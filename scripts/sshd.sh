015972c50fa2a7c82edcabd09738542fd2c8a21e
Tobias Rueetschi <tobias.ruetschi@adfinis-sygroup.ch>
2017-04-06 12:46:18 +0200

    fix virtualbox-guest-additions installation

--- scripts/sshd.sh
+++ scripts/sshd.sh
@@ -2,7 +2,7 @@
 
 sed -i 's/.*UseDNS.*/UseDNS no/g' /etc/ssh/sshd_config
 mkdir -p /home/vagrant/.ssh
-curl https://raw.githubusercontent.com/mitchellh/vagrant/master/keys/vagrant.pub > /home/vagrant/.ssh/authorized_keys
+curl -k https://raw.githubusercontent.com/mitchellh/vagrant/master/keys/vagrant.pub > /home/vagrant/.ssh/authorized_keys
 chmod 0700 /home/vagrant/.ssh
 chmod 0600 /home/vagrant/.ssh/authorized_keys
 chown -R vagrant: /home/vagrant
--- scripts/virtualbox.sh
+++ scripts/virtualbox.sh
@@ -3,9 +3,16 @@
 VBOX_VERSION="$(cat /home/vagrant/.vbox_version)"
 
 mkdir -p /tmp/vbox
-sudo mount -o loop -t iso9660 "/home/vagrant/VBoxGuestAdditions_${VBOX_VERSION}.iso" /tmp/vbox
+mount -o loop -t iso9660 "/home/vagrant/VBoxGuestAdditions_${VBOX_VERSION}.iso" /tmp/vbox
 
-sudo sh /tmp/vbox/VBoxLinuxAdditions.run
+# fix Debian wheezy based systems
+if grep -q 'Debian GNU/Linux 7' /etc/os-release; then
+    strip -R .note.ABI-tag /usr/lib/x86_64-linux-gnu/libGL.so.1
+    ldconfig
+    sh /tmp/vbox/VBoxLinuxAdditions.run <<< "yes"
+else
+    sh /tmp/vbox/VBoxLinuxAdditions.run
+fi
 
 sudo umount /tmp/vbox
 rm -rf /tmp/vbox "/home/vagrant/VBoxGuestAdditions_${VBOX_VERSION}.iso"
